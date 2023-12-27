1630f7cf7d33e604a6e6f41ffc053286f83219ad
TAZIReda <tazir_reda@outlook.fr>
2023-12-27 13:36:35 +0100

    HomeComponent

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -6,5 +6,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  products: any[] = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+{
+  id: '1000',
+  code: 'f230fh0g3',
+  name: 'Bamboo Watch',
+  description: 'Product Description',
+  image: 'bamboo-watch.jpg',
+  price: 65,
+  category: 'Accessories',
+  quantity: 24,
+  inventoryStatus: 'INSTOCK',
+  rating: 5
+},
+{
+  id: '1000',
+  code: 'f230fh0g3',
+  name: 'Bamboo Watch',
+  description: 'Product Description',
+  image: 'bamboo-watch.jpg',
+  price: 65,
+  category: 'Accessories',
+  quantity: 24,
+  inventoryStatus: 'INSTOCK',
+  rating: 5
+},
+{
+  id: '1000',
+  code: 'f230fh0g3',
+  name: 'Bamboo Watch',
+  description: 'Product Description',
+  image: 'bamboo-watch.jpg',
+  price: 65,
+  category: 'Accessories',
+  quantity: 24,
+  inventoryStatus: 'INSTOCK',
+  rating: 5
+},
+{
+  id: '1000',
+  code: 'f230fh0g3',
+  name: 'Bamboo Watch',
+  description: 'Product Description',
+  image: 'bamboo-watch.jpg',
+  price: 65,
+  category: 'Accessories',
+  quantity: 24,
+  inventoryStatus: 'INSTOCK',
+  rating: 5
+},
+  ]; // Fetch from your product list
+  featuredArticles: any[] = []; // Fetch from your article list
+  services: any[] = []; // Fetch from your service list
+  username: string = '';
+  password: string = '';
+
+  responsiveOptions!: any[];
+
+  getSeverity(status: string) {
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+        case 'LOWSTOCK':
+            return 'warning';
+        case 'OUTOFSTOCK':
+            return 'danger';
+        default:
+            return 'info'; // or any default severity you prefer
+    }
+}
 
 }
