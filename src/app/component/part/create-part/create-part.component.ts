a70ee8eb5e90d59c10e150942fd67480159c4e3d
TAZIReda <tazir_reda@outlook.fr>
2023-12-27 14:04:47 +0100

    CreatePartComponent

--- src/app/component/part/create-part/create-part.component.ts
+++ src/app/component/part/create-part/create-part.component.ts
@@ -1,4 +1,18 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-create-part',
+//   templateUrl: './create-part.component.html',
+//   styleUrls: ['./create-part.component.scss']
+// })
+// export class CreatePartComponent {
+
+// }
+
 import { Component } from '@angular/core';
+
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-part',
@@ -6,5 +20,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-part.component.scss']
 })
 export class CreatePartComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+  partForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    partNumber: new FormControl('', Validators.required),
+    quantity: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+    userId: new FormControl('', Validators.required) // Added from Part interface
+    // Add other fields from the Part interface as needed
+  });
 
+  // ... rest of the component code
 }
