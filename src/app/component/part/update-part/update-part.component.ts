2e519bfefbce09389dd09fb4824b46f8fff456ee
TAZIReda <tazir_reda@outlook.fr>
2023-12-27 14:08:17 +0100

    UpdatePartComponent

--- src/app/component/part/update-part/update-part.component.ts
+++ src/app/component/part/update-part/update-part.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-part',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-part.component.scss']
 })
 export class UpdatePartComponent {
-
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+updatePartForm = new FormGroup({
+  name: new FormControl('', Validators.required),
+  description: new FormControl('', Validators.required),
+  partNumber: new FormControl('', Validators.required),
+  quantity: new FormControl('', Validators.required),
+  price: new FormControl('', Validators.required),
+  category: new FormControl('', Validators.required),
+  userId: new FormControl('', Validators.required) // Added from Part interface
+  // Add other fields from the Part interface as needed
+});
 }
